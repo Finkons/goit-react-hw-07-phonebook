3d69530769b9b2688bae1ead8e8f11213ec8f35b
Andrey <finkons87@gmail.com>
2022-08-19 10:43:25 +0300

    change slice

--- src/redux/contactsSlice.jsx
+++ src/redux/contactsSlice.jsx
@@ -3,17 +3,10 @@ import { createSlice } from '@reduxjs/toolkit';
 export const contactsSlice = createSlice({
   name: 'contacts',
   initialState: {
-    items: [],
     filter: '',
   },
 
   reducers: {
-    addContacts(state, action) {
-      state.items.push(action.payload);
-    },
-    deleteContact(state, action) {
-      state.items = state.items.filter(item => item.id !== action.payload);
-    },
     setFilter(state, action) {
       state.filter = action.payload;
     },
